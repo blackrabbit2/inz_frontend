a96911251b5ed0e55d6c9de07e27a8f91870d52f
Weronika Dziedzic <wera02@gmail.com>
2020-12-10 20:56:06 +0100

    Zmiany w footer

--- inz_frontend/src/components/Footer.vue
+++ inz_frontend/src/components/Footer.vue
@@ -1,26 +1,28 @@
 <template>
-  <v-footer dark padless>
+<v-footer dark padless>
     <v-card flat tile app color="primary" class="flex white--text text-center">
-      <v-card-text class="white--text">
-        Politechnika Wrocławska Wydział Elektryczny
-      </v-card-text>
-      <v-card-text class="white--text pt-0">
-        Projekt wykonano w ramach pracy inżynierskiej.
-      </v-card-text>
+        <v-card-text class="white--text ">
+            Politechnika Wrocławska Wydział Elektryczny
+        </v-card-text>
+        <v-divider></v-divider>
 
-      <v-divider></v-divider>
+        <v-card-text class="white--text py-1">
+            Projekt wykonano w ramach pracy inżynierskiej.
+        </v-card-text>
 
-      <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} — <strong>Weronika Dziedzic</strong>
-      </v-card-text>
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+            {{ new Date().getFullYear() }} — <strong>Weronika Dziedzic</strong>
+        </v-card-text>
     </v-card>
-  </v-footer>
+</v-footer>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
-  }),
+    data: () => ({
+        icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+    }),
 };
 </script>
