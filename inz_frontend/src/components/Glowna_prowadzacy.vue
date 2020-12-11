581f2e913cc147ee5967a7724a6cd51f177b36eb
Weronika Dziedzic <wera02@gmail.com>
2020-12-11 13:33:45 +0100

    Dodanie opcji usuwania przedmiotow

--- inz_frontend/src/components/Glowna_prowadzacy.vue
+++ inz_frontend/src/components/Glowna_prowadzacy.vue
@@ -3,7 +3,20 @@
     <Dodaj_przedmiot />
     <v-expansion-panels focusable>
       <v-expansion-panel v-for="przedmiot in przedmioty" :key="przedmiot.id">
-        <v-expansion-panel-header v-text="przedmiot.nazwa" />
+        <v-expansion-panel-header>
+        <v-row justify="space-between">
+        <v-col cols="9" sm="5">
+        {{przedmiot.nazwa}}
+        </v-col>
+        <v-col cols="3" sm="1" v-if="przedmiot">
+                <Usun_przedmiot
+                  @przedmiotUsun="przedmiot_init"
+                  :przedmiot="przedmiot"
+                  :key="przedmiot.id"
+                />
+              </v-col>
+        </v-row>
+        </v-expansion-panel-header>
         <v-expansion-panel-content>
           <!-- Nazwa componentu, który wstawiamy w tym miejscu :props='obiekt,który chcemy przekazać' -->
           <Grupy :przedmiot="przedmiot" />
@@ -16,6 +29,7 @@
 <script>
 import Grupy from "./Grupy";
 import Dodaj_przedmiot from "./Dodaj_przedmiot";
+import Usun_przedmiot from "./Usun_przedmiot"
 
 export default {
   name: "glowna_prowadzacy",
@@ -24,6 +38,7 @@ export default {
   components: {
     Grupy: Grupy,
     Dodaj_przedmiot: Dodaj_przedmiot,
+    Usun_przedmiot: Usun_przedmiot,
   },
 
   data: () => ({
@@ -34,6 +49,14 @@ export default {
     this.$api.get("przedmioty/").then((response) => {
       this.przedmioty = response.data;
     });
+  },
+  methods: {
+    przedmiot_init(){
+         this.$api.get("przedmioty/")
+         .then((response) => {
+      this.przedmioty = response.data;
+    });
+    }
   },
 };
 </script>
---
+++ inz_frontend/src/components/Usun_przedmiot.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn dark color="secondary" text small @click="usun(przedmiot)">
+    <v-icon>fas fa-trash</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "usun_przedmiot",
+  props: ["przedmiot"],
+
+  methods: {
+    usun: function (przedmiot) {
+      if (confirm("Na pewno usunąć przedmiot " + przedmiot.nazwa)) {
+        this.$api.delete("/przedmioty/" + przedmiot.id + "/").then(() => {
+          przedmiot.id = 0;
+          this.$emit("przedmiotUsun");
+        });
+      }
+    },
+  },
+};
+</script>
